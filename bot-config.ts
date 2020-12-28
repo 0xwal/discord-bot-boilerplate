7d23753c13e500bf2fda3028f17b457d0a992921
0xWaleed <imwaleed@outlook.sa>
2020-12-28 21:35:09 +0300

    change secret.json with env based file
    Using .env over the secret.json file
    to hold secret data is better and more
    flexible to manage.

--- bot-config.ts
+++ bot-config.ts
@@ -1,6 +1,13 @@
-import secret from './.secret.json';
+function getToken(): string
+{
+    let botToken = process.env.BOT_TOKEN;
+    if (botToken) {
+        return botToken;
+    }
+    return '';
+}
 
 export const botConfig: BotConfig = {
     prefix: '^',
-    token: secret.token
+    token: getToken()
 };
